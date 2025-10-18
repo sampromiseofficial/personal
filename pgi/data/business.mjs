16c55bf47ca5156dd1b24fdee12d37fe2af3eb21
sampromiseofficial <sampromiseofficial@gmail.com>
2025-10-18 02:53:21 +0100

    G2G

--- pgi/data/business.mjs
+++ pgi/data/business.mjs
@@ -4,7 +4,7 @@ export const businessGroups = [
         "title": "Psalmtech ",
         "address": "123 City Rd",
         "image": "images/psalmtech.webp",
-        "learn_more": "https://www.psalmtech.com",
+        "learn_more": "https://wa.me/2347085952071",
         "description": "A leading tech company specializing in innovative solutions."
 
     },
@@ -14,7 +14,7 @@ export const businessGroups = [
         "title": "Psalm Auto",
         "address": "45 Old Town Sq",
         "image": "images/toyota.webp",
-        "learn_more": "https://www.historicmarket.com",
+        "learn_more": "https://wa.me/2347085952071",
         "description": "Local market with fresh produce, crafts, and weekend events."
     },
     
@@ -23,7 +23,7 @@ export const businessGroups = [
         "title": "Wakanow",
         "address": "10 Heritage Ln",
         "image": "images/wakanow.webp",
-        "learn_more": "https://www.townmuseum.com",
+        "learn_more": "https://wa.me/2347085952071",
         "description": "Exhibits about the area's history and notable figures."
     },
     
@@ -32,7 +32,7 @@ export const businessGroups = [
         "title": "TravelStart",
         "address": "Gallery Row",
         "image": "images/travelstart.webp",
-        "learn_more": "https://www.artwalk.com",
+        "learn_more": "https://www.travelstart.com.ng/?affId=222484&utm_source=affiliate&utm_medium=222484",
         "description": "A collection of galleries and street murals celebrating local artists."
     },
     
@@ -41,7 +41,7 @@ export const businessGroups = [
         "title": "Exness trade",
         "address": "88 Discovery Dr",
         "image": "images/exness.webp",
-        "learn_more": "https://www.sciencecentre.com",
+        "learn_more": "https://one.exnesstrack.org/a/c_8sag0lfh2u",
         "description": "Hands-on exhibits for families and school groups."
     },
 
